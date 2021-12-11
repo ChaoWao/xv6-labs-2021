bec096acfebd73222db2c36fbb332ee5fe57a94d
Chao Wang <wcwxy@mail.ustc.edu.cn>
2021-12-11 14:25:51 +0800

    feat: backtrace passed

--- kernel/printf.c
+++ kernel/printf.c
@@ -132,3 +132,15 @@ printfinit(void)
   initlock(&pr.lock, "pr");
   pr.locking = 1;
 }
+
+// backtrace
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 base = PGROUNDUP(fp);
+  while (fp != base) {
+    printf("%p\n", *((uint64 *)(fp - 8)));
+    fp = *((uint64 *)(fp - 16));
+  }
+}
