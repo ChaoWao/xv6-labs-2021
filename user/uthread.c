a8a4c71275575e823178d2ab7e80a630ef5ac40f
Chao Wang <wcwxy@mail.ustc.edu.cn>
2021-12-19 19:01:22 +0800

    feat: uthread passed

--- user/uthread.c
+++ user/uthread.c
@@ -62,6 +62,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch((uint64)(next_thread->stack), (uint64)(t->stack));
   } else
     next_thread = 0;
 }
@@ -76,6 +77,9 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  uint64 * p = (uint64 *)(t->stack + STACK_SIZE);
+  *(p - 13) = (uint64)func;
+  *((uint64 *)(t->stack)) = (uint64)(p - 13);
 }
 
 void 
